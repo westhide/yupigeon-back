d1e7e5c236cc9220011032f373894d7dcf148988
westhide <mr.westhide@gmail.com>
2022-03-31 00:07:37 +0800

    fix: offline_conductor daily_receipt

--- database/src/entity/ship_ticket_bill/offline_conductor_daily_receipt.rs
+++ database/src/entity/ship_ticket_bill/offline_conductor_daily_receipt.rs
@@ -5,7 +5,10 @@ use serde::{Deserialize, Serialize};
 pub struct OfflineConductorDailyReceipt {
     date: Date,
     user_name: String,
-    amount: Option<Decimal>,
+    receipt: Option<Decimal>,
+    sum_pay_amount: Option<Decimal>,
+    sum_fee: Option<Decimal>,
+    sum_refund_amount: Option<Decimal>,
 }
 
 pub async fn offline_conductor_daily_receipt(
@@ -16,20 +19,66 @@ pub async fn offline_conductor_daily_receipt(
     OfflineConductorDailyReceipt::find_by_statement(Statement::from_sql_and_values(
         txn.get_database_backend(),
         r#"
+            WITH offr AS (
+                SELECT
+                    DATE( refund_finish_time ) date,
+                    user_name,
+                    SUM(IF(refund_amount>0,refund_amount+fee,0)) sum_refund_amount,
+                    SUM(fee) sum_fee
+                FROM ship_ticket_refund_bill
+                WHERE
+                        user_type='线下'
+                        AND refund_finish_time >=? AND refund_finish_time <= ?
+                GROUP BY date,user_name
+            )
+            ,offp AS (
+                SELECT
+                        DATE( create_time ) date,
+                        user_name,
+                        SUM( pay_amount ) sum_pay_amount
+                FROM ticket_bill
+                WHERE
+                        user_type='线下'
+                        AND pay_amount IS NOT NULL
+                        AND create_time >=? AND create_time <= ?
+                GROUP BY date,user_name
+            )
+            ,off AS (
             SELECT
-                DATE( create_time ) date,
-                user_name,
-                SUM( pay_amount ) amount
-            FROM ticket_bill
-            WHERE
-                user_type='线下'
-                AND pay_amount IS NOT NULL
-                AND create_time >=? AND create_time <= ?
-            GROUP BY date,user_name
-            ORDER BY date DESC,user_name
+                offp.date,
+                offp.user_name,
+                offp.sum_pay_amount,
+                offr.sum_fee,
+                offr.sum_refund_amount
+            FROM offp LEFT JOIN offr ON offp.date=offr.date AND offp.user_name=offr.user_name
+            UNION
+            SELECT
+                offr.date,
+                offr.user_name,
+                offp.sum_pay_amount,
+                offr.sum_fee,
+                offr.sum_refund_amount
+            FROM offp RIGHT JOIN offr ON offp.date=offr.date AND offp.user_name=offr.user_name
+            )
+            SELECT
+                off.date,
+                off.user_name,
+                IFNULL(off.sum_pay_amount,0) + IFNULL(off.sum_fee,0) receipt,
+                off.sum_pay_amount,
+                off.sum_fee,
+                off.sum_refund_amount
+            FROM
+                off
+            WHERE off.sum_pay_amount>0 OR off.sum_fee>0 OR off.sum_refund_amount>0
+            ORDER BY off.date DESC,off.user_name
         ;
         "#,
-        vec![datetime_from.into(), datetime_end.into()],
+        vec![
+            datetime_from.into(),
+            datetime_end.into(),
+            datetime_from.into(),
+            datetime_end.into(),
+        ],
     ))
     .all(&txn)
     .await
