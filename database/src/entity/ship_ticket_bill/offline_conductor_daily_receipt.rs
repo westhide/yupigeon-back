7d61f28bccccef55756a779e74538d14520d8b8e
westhide <mr.westhide@gmail.com>
2022-04-02 13:33:37 +0800

    feat: add fee detial

--- database/src/entity/ship_ticket_bill/offline_conductor_daily_receipt.rs
+++ database/src/entity/ship_ticket_bill/offline_conductor_daily_receipt.rs
@@ -8,11 +8,13 @@ pub struct OfflineConductorDailyReceipt {
     receipt: Option<Decimal>,
     sum_pay_amount: Option<Decimal>,
     sum_fee: Option<Decimal>,
+    just_refund_fee: Option<Decimal>,
+    just_change_fee: Option<Decimal>,
     sum_refund_amount: Option<Decimal>,
-    just_refund_total: Option<Decimal>,
-    just_change_total: Option<Decimal>,
-    just_compensation_total: Option<Decimal>,
-    just_hcbb_total: Option<Decimal>,
+    just_refund_amount: Option<Decimal>,
+    just_change_amount: Option<Decimal>,
+    just_compensation_amount: Option<Decimal>,
+    just_hcbb_amount: Option<Decimal>,
 }
 
 pub async fn offline_conductor_daily_receipt(
@@ -28,12 +30,14 @@ pub async fn offline_conductor_daily_receipt(
                     DATE( refund_finish_time ) date,
                     user_name,
                     SUM(IF(refund_amount>0,refund_amount+fee,0)) sum_refund_amount,
-                    SUM(IF(refund_type='已退款',IF(refund_amount>0,refund_amount+fee,0),0)) just_refund_total,
-                    SUM(IF(refund_type='改签废票',IF(refund_amount>0,refund_amount+fee,0),0)) just_change_total,
-                    SUM(IF(refund_type='已补差',IF(refund_amount>0,refund_amount+fee,0),0)) just_compensation_total,
-                    SUM(IF(refund_type='已换船',IF(refund_amount>0,refund_amount+fee,0),0)) just_hcbb_total,
+                    SUM(IF(refund_type='已退款',IF(refund_amount>0,refund_amount+fee,0),0)) just_refund_amount,
+                    SUM(IF(refund_type='改签废票',IF(refund_amount>0,refund_amount+fee,0),0)) just_change_amount,
+                    SUM(IF(refund_type='已补差',IF(refund_amount>0,refund_amount+fee,0),0)) just_compensation_amount,
+                    SUM(IF(refund_type='已换船',IF(refund_amount>0,refund_amount+fee,0),0)) just_hcbb_amount,
                     SUM(IF(refund_amount>0,fee,0)) sum_repeat_fee,
-                    SUM(fee) sum_fee
+                    SUM(fee) sum_fee,
+                    SUM(IF(refund_type='已退款',fee,0)) just_refund_fee,
+                    SUM(IF(refund_type='改签废票',fee,0)) just_change_fee
                 FROM ship_ticket_refund_bill
                 WHERE
                         user_type='线下'
@@ -58,11 +62,13 @@ pub async fn offline_conductor_daily_receipt(
                 offp.user_name,
                 offp.sum_pay_amount,
                 offr.sum_fee,
+                offr.just_refund_fee,
+                offr.just_change_fee,
                 offr.sum_refund_amount,
-                offr.just_refund_total,
-                offr.just_change_total,
-                offr.just_compensation_total,
-                offr.just_hcbb_total,
+                offr.just_refund_amount,
+                offr.just_change_amount,
+                offr.just_compensation_amount,
+                offr.just_hcbb_amount,
                 offr.sum_repeat_fee
             FROM offp LEFT JOIN offr ON offp.date=offr.date AND offp.user_name=offr.user_name
             UNION
@@ -71,11 +77,13 @@ pub async fn offline_conductor_daily_receipt(
                 offr.user_name,
                 offp.sum_pay_amount,
                 offr.sum_fee,
+                offr.just_refund_fee,
+                offr.just_change_fee,
                 offr.sum_refund_amount,
-                offr.just_refund_total,
-                offr.just_change_total,
-                offr.just_compensation_total,
-                offr.just_hcbb_total,
+                offr.just_refund_amount,
+                offr.just_change_amount,
+                offr.just_compensation_amount,
+                offr.just_hcbb_amount,
                 offr.sum_repeat_fee
             FROM offp RIGHT JOIN offr ON offp.date=offr.date AND offp.user_name=offr.user_name
             )
@@ -85,11 +93,13 @@ pub async fn offline_conductor_daily_receipt(
                 IFNULL(off.sum_pay_amount,0) + IFNULL(off.sum_repeat_fee,0) receipt,
                 off.sum_pay_amount,
                 IF(off.sum_fee>0,off.sum_fee,NULL) sum_fee,
+                IF(off.just_refund_fee>0,off.just_refund_fee,NULL) just_refund_fee,
+                IF(off.just_change_fee>0,off.just_change_fee,NULL) just_change_fee,
                 IF(off.sum_refund_amount>0,off.sum_refund_amount,NULL) sum_refund_amount,
-                IF(off.just_refund_total>0,off.just_refund_total,NULL) just_refund_total,
-                IF(off.just_change_total>0,off.just_change_total,NULL) just_change_total,
-                IF(off.just_compensation_total>0,off.just_compensation_total,NULL) just_compensation_total,
-                IF(off.just_hcbb_total>0,off.just_hcbb_total,NULL) just_hcbb_total
+                IF(off.just_refund_amount>0,off.just_refund_amount,NULL) just_refund_amount,
+                IF(off.just_change_amount>0,off.just_change_amount,NULL) just_change_amount,
+                IF(off.just_compensation_amount>0,off.just_compensation_amount,NULL) just_compensation_amount,
+                IF(off.just_hcbb_amount>0,off.just_hcbb_amount,NULL) just_hcbb_amount
             FROM
                 off
             WHERE off.sum_pay_amount>0 OR off.sum_fee>0 OR off.sum_refund_amount>0
