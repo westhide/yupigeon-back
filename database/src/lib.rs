299756a89d6a21ebdf79eeeaf4e6cb85784c5f80
westhide <mr.westhide@gmail.com>
2022-05-04 14:32:04 +0800

    rename: database.rs to base.rs

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -3,10 +3,10 @@
 // @Last Modified by:   westhide.yzw
 // @Last Modified time: 2022-03-20 16:15:05
 
+mod base;
 mod config;
-mod database;
 pub mod entity;
 pub mod query;
 
-pub use database::Database;
+pub use base::Database;
 pub use sea_orm;
